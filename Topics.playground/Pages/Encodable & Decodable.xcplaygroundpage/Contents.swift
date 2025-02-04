05aab140e22c61d8f6cde14c8871c98446f9382f
Rahul Gangwar <rahul.gangwar@hastree.com>
2025-02-04 14:09:30 +0530

    new topic added

---
+++ Topics.playground/Pages/Encodable & Decodable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+/*
+ MARK: - Encodable & Decodable
+ Encodable and Decodable are protocols used for encoding (converting objects to JSON or other formats) and decoding (parsing JSON into objects).
+ 
+ 
+ */
+
+
+import Foundation
+
+var greeting = "Encodable & Decodable"
+
